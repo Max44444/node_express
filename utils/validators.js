7557e2df88f6737821c9d27433665e47882d2c69
Max44444 <Skorokhodov.m.v@ukr.net>
2021-01-27 10:35:24 +0200

    normalize registration data

--- utils/validators.js
+++ utils/validators.js
@@ -2,22 +2,32 @@ const { body } = require('express-validator/check');
 const User = require('../models/User');
 
 exports.registerValidators = [
-  body('email', 'Enter correct email').isEmail().custom(async (value, { req }) => {
-    try {
-      const user = await User.findOne({ email: value });
-      if (user) {
-        return Promise.reject('User with this email already exist');
+  body('email', 'Enter correct email')
+    .isEmail()
+    .custom(async (value, { req }) => {
+      try {
+        const user = await User.findOne({ email: value });
+        if (user) {
+          return Promise.reject('User with this email already exist');
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  }),
-  body('password', 'Password should be min 6 signs').isLength({ min: 6, max: 56 }).isAlphanumeric(),
-  body('confirm').custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error('Passwords must match')
-    }
-    return true;
-  }),
-  body('name', 'Name shoud be min 3 signs').isLength({ min: 3 })
+    })
+    .normalizeEmail(),
+  body('password', 'Password should be min 6 signs')
+    .isLength({ min: 6, max: 56 })
+    .isAlphanumeric()
+    .trim(),
+  body('confirm')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords must match')
+      }
+      return true;
+    })
+    .trim(),
+  body('name', 'Name shoud be min 3 signs')
+    .isLength({ min: 3 })
+    .trim()
 ]
